1501a5d562eb05a6f14650765eab942a554a77c7
Elex <68489652+elex-project@users.noreply.github.com>
2021-01-28 15:23:06 +0900

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,8 +6,8 @@ plugins {
 }
 
 group = "com.elex-project"
-version = "1.0-SNAPSHOT"
-description = ""//todo
+version = "1.2.0"
+description = "Math Expression String Parser"
 
 repositories {
 	maven {
@@ -32,12 +32,12 @@ configurations {
 }
 
 tasks.jar {
-	manifest { // todo
+	manifest { 
 		attributes(mapOf(
 				"Implementation-Title" to project.name,
 				"Implementation-Version" to project.version,
 				"Implementation-Vendor" to "ELEX co.,pte.",
-				"Automatic-Module-Name" to "com.elex_project.${project.name}"
+				"Automatic-Module-Name" to "com.elex_project.merlion"
 		))
 	}
 }
@@ -72,21 +72,15 @@ publishing {
 				// todo
 				name.set(project.name)
 				description.set(project.description)
-				url.set("https://")
-				properties.set(mapOf(
-						"myProp" to "value",
-						"prop.with.dots" to "anotherValue"
-				))
+				url.set("https://github.com/elex-project/merlion")
 				organization {
 					name.set("Elex co.,Pte.")
 					url.set("https://www.elex-project.com/")
 				}
 				licenses {
 					license {
-						// todo
 						name.set("BSD 3-Clause License")
-						url.set("licenseUrl")
-						comments.set("")
+						url.set("https://github.com/elex-project/merlion/blob/main/LICENSE")
 					}
 				}
 				developers {
@@ -99,21 +93,13 @@ publishing {
 						organizationUrl.set("https://www.elex-project.com/")
 						roles.set(arrayListOf("Developer", "CEO"))
 						timezone.set("Asia/Seoul")
-						properties.set(mapOf("" to ""))
-					}
-				}
-				contributors {
-					contributor {
-						name.set("")
-						email.set("")
-						url.set("")
 					}
 				}
 				scm {
 					// todo
-					connection.set("scm:git:https://github.com/my-library.git")
-					developerConnection.set("scm:git:https://github.com/my-library.git")
-					url.set("https://github.com/my-library/")
+					connection.set("scm:git:https://github.com/elex-project/merlion.git")
+					developerConnection.set("scm:git:https://github.com/elex-project/merlion.git")
+					url.set("https://github.com/elex-project/merlion")
 				}
 			}
 		}
@@ -133,7 +119,7 @@ publishing {
 		}
 		maven { //todo
 			name = "mavenGithub"
-			url = uri("https://maven.pkg.github.com/elex-project/tmpl-java-library")
+			url = uri("https://maven.pkg.github.com/elex-project/merlion")
 			credentials {
 				username = project.findProperty("github.username") as String
 				password = project.findProperty("github.token") as String
@@ -143,7 +129,6 @@ publishing {
 }
 
 dependencies {
-	implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
 	implementation("org.slf4j:slf4j-api:1.7.30")
 	implementation("org.jetbrains:annotations:20.1.0")
 
